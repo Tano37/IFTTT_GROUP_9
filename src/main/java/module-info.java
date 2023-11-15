b47cb12363d770d630d309a83bfab4a11aba67ab
Tano37 <g.rea7@studenti.unisa.it>
2023-11-15 16:34:39 +0100

    1COMMIT

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.unisa.ifttt_group_9 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.unisa.ifttt_group_9 to javafx.fxml;
+    exports it.unisa.ifttt_group_9;
+}
