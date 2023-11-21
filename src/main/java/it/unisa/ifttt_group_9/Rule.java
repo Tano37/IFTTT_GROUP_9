cfa9852fb347a8d1b5b81c0982748020a230befa
Tydeeee <125651236+Tydeeee@users.noreply.github.com>
2023-11-21 19:00:19 +0100

    timestamp triggers definition
    definition of the timestamp trigger class, plus

---
+++ src/main/java/it/unisa/ifttt_group_9/Rule.java
@@ -0,0 +1,9 @@
+package it.unisa.ifttt_group_9;
+
+public class Rule {
+    String ruleName;
+    Trigger trigger;
+    Action action;
+
+
+}
---
+++ src/main/java/it/unisa/ifttt_group_9/TriggerTimestamp.java
@@ -0,0 +1,33 @@
+package it.unisa.ifttt_group_9;
+
+import it.unisa.ifttt_group_9.exceptions.IllegalTimeException;
+
+import java.time.LocalTime;
+
+public class TriggerTimestamp implements Trigger {
+    int hour;
+    int minute;
+
+    public TriggerTimestamp (int hour, int minute) throws IllegalTimeException {
+        if ((hour < 0 || hour > 23) || (minute < 0 || minute > 59) )
+            throw new IllegalTimeException();
+        this.hour=hour;
+        this.minute=minute;
+    }
+
+    /*
+    public void setTime (int hour, int minute){
+        this.hour=hour;
+        this.minute=minute;
+    }
+    */
+
+    @Override
+    public boolean evaluate(){
+        LocalTime now= LocalTime.now();
+        int h= now.getHour();
+        int m= now.getMinute();
+        return (h==this.hour && m==this.minute);
+    }
+
+}
