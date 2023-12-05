39164f4c59163297768052f3d1c86b827c44f1d8
giuseppemarotta <150940815+giuseppemarotta@users.noreply.github.com>
2023-12-05 12:36:08 +0100

    Create TriggerExecuteService.java

---
+++ src/main/java/it/unisa/ifttt_group_9/Trigger/TriggerExecuteService.java
@@ -0,0 +1,37 @@
+package it.unisa.ifttt_group_9.Trigger;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+import java.util.Arrays;
+
+public class TriggerExecuteService  extends Service<Void> {
+    private TriggerExitStatus triggerSelected;
+
+    public TriggerExecuteService(TriggerExitStatus triggerSelected) {
+        this.triggerSelected = triggerSelected;
+    }
+
+
+    @Override
+    protected Task<Void> createTask() {
+        return new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                int exitStatus1;
+
+                ProcessBuilder processBuilder = new ProcessBuilder();
+                //System.out.println(triggerSelected.getStringPath()+ triggerSelected.getCommandLine());
+                // Aggiungere il percorso dell'eseguibile e gli eventuali argomenti
+                processBuilder.command(Arrays.asList(triggerSelected.getStringPath(), triggerSelected.getCommandLine()));
+
+                // Eseguire il processo
+                Process process = processBuilder.start();
+                triggerSelected.setExitValue(process.waitFor());
+
+
+                return null;
+            }
+        };
+    }
+}
