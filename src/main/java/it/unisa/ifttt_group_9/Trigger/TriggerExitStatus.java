ff36b04963bc66a54c5f8fd97bc12295b2a2ab15
giuseppemarotta <150940815+giuseppemarotta@users.noreply.github.com>
2023-12-05 12:34:31 +0100

    Update TriggerExitStatus.java

--- src/main/java/it/unisa/ifttt_group_9/Trigger/TriggerExitStatus.java
+++ src/main/java/it/unisa/ifttt_group_9/Trigger/TriggerExitStatus.java
@@ -1,4 +1,54 @@
 package it.unisa.ifttt_group_9.Trigger;
 
-public class TriggerExitStatus {
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class TriggerExitStatus implements Trigger {
+    private String stringPath;
+    private String commandLine;
+    private int exitExpected;
+    private int exitValue; // Variabile di istanza per memorizzare il valore restituito
+
+    public TriggerExitStatus(String stringPath, String commandLine, int exitStatus) {
+        this.stringPath = stringPath;
+        this.commandLine = commandLine;
+        this.exitExpected = exitStatus;
+    }
+
+
+    public void setExitValue(int exitValue) {
+        this.exitValue = exitValue;
+    }
+
+    public String getStringPath() {
+        return stringPath;
+    }
+
+    public String getCommandLine() {
+        return commandLine;
+    }
+
+    public int getExitExpected() {
+        return exitExpected;
+    }
+
+    public int getExitValue() {
+        return exitValue;
+    }
+
+    @Override
+    public boolean evaluate() {
+        AtomicBoolean evaluationResult = new AtomicBoolean(false);
+
+        TriggerExecuteService myTrigger = new TriggerExecuteService(this);
+        myTrigger.setOnSucceeded(e -> {
+
+            //System.out.println("Risultato esecuzione: "+ this.getExitValue()+"risultato confrontato="+ this.getExitExpected());
+
+        });
+        myTrigger.start();
+
+
+
+        return this.getExitExpected()==this.getExitValue();
+    }
 }
